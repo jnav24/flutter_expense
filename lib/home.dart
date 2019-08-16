abd2e0dee7736086e5366c2a1d8833f6ec979e1e
Justin Navarro <info@justinnavarro.net>
2019-08-16 18:17:26 -0400

    moved transaction class

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import './transaction.dart';
+import 'package:flutter_expense/models/transaction.dart';
 
 class Home extends StatelessWidget {
 	@override
